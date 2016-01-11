a2607a7e84579dcd66da0a0f27aedbc7f13f6685
yinyumeng <yinym3@gmail.com>
2016-01-11 16:00:58 +0800

    update the experiment file

--- hyperParamTuning/HyperParamTuning.py
+++ hyperParamTuning/HyperParamTuning.py
@@ -124,6 +124,7 @@ def main():
 
     project_dir = os.environ['EXPERI_PROJECT_PATH']
     sys.path.append(project_dir)
+    sys.path.append(project_dir+"library/spearmint")
 
     log("experiment_config is: " + args[0]);
 
