3c81c8e78c54447685d517a18f050554759d008d
yinyumeng <yinym3@gmail.com>
2016-02-03 13:23:38 +0800

    update bayesian optimization module

--- BayOptBNN/experiment/GP/big_data_optimization/1000meta/big_data_optimization.py
+++ BayOptBNN/experiment/GP/big_data_optimization/1000meta/big_data_optimization.py
@@ -4,10 +4,10 @@
 
 
 """
-This is a simple demo to demonstrate the use of Bayesian optimization with BO_BNN with using sparse GPs. Run the example by writing:
+This is a simple demo to demonstrate the use of Bayesian optimization with GPyOpt with using sparse GPs. Run the example by writing:
 
-import BO_BNN
-BO_demo_big_data = BO_BNN.demos.big_data_optimization()
+import GPyOpt
+BO_demo_big_data = GPyOpt.demos.big_data_optimization()
 
 As a result you should see:
 
@@ -22,36 +22,27 @@ and to the location of the best found location writing.
 BO_demo_big_data.x_opt
 
 """
-import os
-import sys
-# project_dir = os.environ['EXPERI_PROJECT_PATH']
-sys.path.append("/home/jie/d2")
-sys.path.append("/home/jie/d2/hyperParam")
-sys.path.append("/home/jie/d2/hyperParam/BayOptBNN")
-sys.path.append("/home/jie/d2/hyperParam/bayesianneuralnetwork")
 
 def big_data_optimization(plots=True):
-    import BayOptBNN
+    import GPyOpt
     from numpy.random import seed
     seed(12345)
     
     # --- Objective function
-    objective_noisy = BayOptBNN.fmodels.experimentsNd.alpine2(5,sd = 0.1)     # Alpine2 function in dimension 5.
+    objective_noisy = GPyOpt.fmodels.experimentsNd.alpine2(5,sd = 0.1)     # Alpine2 function in dimension 10.
     bounds = objective_noisy.bounds                                         # problem constrains 
 
     # --- Problem definition and optimization
-    BO_demo_big_data = BayOptBNN.methods.BayesianOptimizationBNN(f=objective_noisy.f,  # function to optimize
+    BO_demo_big_data = GPyOpt.methods.BayesianOptimization(f=objective_noisy.f,  # function to optimize       
                                             bounds=bounds,                 # box-constrains of the problem
                                             acquisition='LCB',             # Selects the Lower Confidence Bound criterion
                                             acquisition_par = 2,           # parameter of the acquisition function
-                                            normalize = True,              # normalized acquisition function
-                                            layer_sizes =[5, 30, 30, 1],
+                                            normalize = True,              # normalized acquisition function      
+                                            sparseGP = True,               # Use a sparse GP for the sparse GP.
+                                            num_inducing = 10,             # Number of inducing points
                                             numdata_initial_design = 10)        # Initialize the model with 1000 points
-
-
-
-    # Run the optimization:
-    #it will generate one new training sample using the function every mate-iteration
+    
+    # Run the optimization
     max_iter = 1000
 
     print '-----'
@@ -66,11 +57,10 @@ def big_data_optimization(plots=True):
                                 true_gradients = False)                     # The gradients of the acquisition function are approximated (faster)
     # --- Plots
     if plots:
-        BO_demo_big_data.plot_convergence("bnn_covergence_final_inputdim1000.pdf",full=True)
-    BO_demo_big_data.save_report("bnn_report_final_inputdim1000.pdf")
+        BO_demo_big_data.plot_convergence("gp_covergence_final_inputdim5.pdf")
+    BO_demo_big_data.save_report()
     BO_demo_big_data.save_result()
+    
     return BO_demo_big_data
-
-
 if __name__=='__main__':
     big_data_optimization()
